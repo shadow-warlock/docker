5b016c80a9a8c2a9af198954057d4ee14f481514
nik <nik_mak@bk.ru>
2020-08-04 19:29:10 +0300

    fix access to volumes

--- project_creator/create.sh
+++ project_creator/create.sh
@@ -4,5 +4,6 @@ mkdir "$1"
 cp -r project_creator/docker "$1"
 cp -r project_creator/project "$1"
 cp -r project_creator/docker-compose.yaml "$1"
+cp -r project_creator/.env "$1"
 rm "$1"/project/backend/.root
 rm "$1"/project/frontend/.root
